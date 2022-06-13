bb4c106871e9d81a9b63fe4219ad2e89a9089190
Fabrício Lima Carvalho <105167072+IFafaa@users.noreply.github.com>
2022-06-13 17:38:18 -0300

    criando as pages
    criando as pastas com as pages em que cada um vai trabalhar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TransactionComponent } from './pages/transaction/transaction.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingComponent,
+    LoginComponent,
+    TransactionComponent,
   ],
   imports: [
     BrowserModule,

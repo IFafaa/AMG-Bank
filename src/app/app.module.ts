fb7d4787e68cbc0779c998448450fa76d31a6ac0
Fabrício Lima Carvalho <105167072+IFafaa@users.noreply.github.com>
2022-06-13 17:35:00 -0300

    deleting archives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { TransactionPageComponent } from './components/transaction-page/transaction-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    LoginPageComponent,
-    TransactionPageComponent
   ],
   imports: [
     BrowserModule,

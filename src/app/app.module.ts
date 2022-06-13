8051cf2060af442cd3eafaee387907b00b5668cc
Fabrício Lima Carvalho <105167072+IFafaa@users.noreply.github.com>
2022-06-13 16:54:19 -0300

    criação dos componentes principais
    landing-page, login-page, transaction-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { TransactionPageComponent } from './components/transaction-page/transaction-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent,
+    TransactionPageComponent
   ],
   imports: [
     BrowserModule,

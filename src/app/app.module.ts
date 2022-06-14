8bf07ad63bd74d7e27aee889669cc4a12a00f542
oGeanCosta <101133555+oGeanCosta@users.noreply.github.com>
2022-06-14 10:41:29 -0300

    routes and lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { LoginComponent } from './pages/login/login.component';
-import { TransactionComponent } from './pages/transaction/transaction.component';
+import { PagesModule } from './pages/pages.module';
+// import { LandingComponent } from './pages/landing/landing.component';
+// import { LoginComponent } from './pages/login/login.component';
+// import { TransactionComponent } from './pages/transaction/transaction.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
-    LoginComponent,
-    TransactionComponent,
+    // LandingComponent,
+    // LoginComponent,
+    // TransactionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]

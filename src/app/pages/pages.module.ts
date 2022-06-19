1d413928c1a6d05641cabe6be8d97e280481da21
GGeras <104459038+GGeras@users.noreply.github.com>
2022-06-19 05:15:12 -0300

    Erro de import pages.module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,10 +5,10 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { NavBarComponent } from './nav-bar-buttom/nav-bar.component';
-import { TransactionTesteComponent } from './transaction-teste/transaction-teste.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { NavBarTopComponent } from './nav-bar-top/nav-bar-top.component';
 import { CardsComponent } from './cards/cards.component';
+import { TransactionTesteComponent } from './transaction-teste/transaction-teste.component';
 
 
 @NgModule({
